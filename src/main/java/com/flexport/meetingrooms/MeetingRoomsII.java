943721e1ea27f9dcbddd953bae21d2f6f8032832
caibing0790 <153341928+caibing0790@users.noreply.github.com>
2025-02-04 23:33:16 +0800

    complete meeting rooms

--- src/main/java/com/flexport/meetingrooms/MeetingRoomsII.java
+++ src/main/java/com/flexport/meetingrooms/MeetingRoomsII.java
@@ -1,7 +1,24 @@
 package com.flexport.meetingrooms;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class MeetingRoomsII {
     public int minMeetingRooms(int[][] intervals) {
-        return -1;
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for (int[] interval : intervals) {
+            treeMap.put(interval[0], treeMap.getOrDefault(interval[0], 0) + 1);
+            treeMap.put(interval[1], treeMap.getOrDefault(interval[1], 0) - 1);
+        }
+
+        int minRoomNeed = 0;
+        int roomNeed = 0;
+        for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
+            int value = entry.getValue();
+            roomNeed += value;
+            minRoomNeed = Math.max(minRoomNeed, roomNeed);
+        }
+
+        return minRoomNeed;
     }
 }
